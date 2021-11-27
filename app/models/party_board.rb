fdae7627b8cab1d3108c2c87843242d96e5a7888
井上涼太 <inoueryouta@inoueryoutas-MacBook-Air.local>
2021-11-27 22:11:50 +0900

    Add: scss

--- app/models/party_board.rb
+++ app/models/party_board.rb
@@ -2,6 +2,6 @@ class PartyBoard < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 255 }
-  validates :body, presence: true, length: { maximum: 65_535 }
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 400 }
 end
